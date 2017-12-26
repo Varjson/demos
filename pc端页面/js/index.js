6af7d94f1f18bd220a41ef3137e53d9d0af55bec
Varjson <json_key@163.com>
2017-12-26 14:10:43 +0800

    new

--- pc端页面/js/index.js
+++
@@ -1,7 +0,0 @@
-$('.nav li').mouseover(function(){
-				$(this).addClass('active').siblings().removeClass('active');
-		});
-		
-if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
-        new WOW().init();
-    };
