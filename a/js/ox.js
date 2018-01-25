f3256e44de58404f78e7a5c5116c0f0838f61c1b
Varjson <json_key@163.com>
2018-01-25 21:41:47 +0800

    o
    o

--- a/js/ox.js
+++ a/js/ox.js
@@ -1,6 +1,6 @@
 !function(a,b){var c=a.documentElement,d="orientationchange"in window?"orientationchange":"resize",e=function(){var a=c.clientWidth;a&&(c.style.fontSize=100*(a/1242)+"px")};a.addEventListener&&(b.addEventListener(d,e,!1),a.addEventListener("DOMContentLoaded",e,!1))}(document,window);
 window.onload = function() {
-	var Navlist = document.querySelectorAll(".header-nav li");
+	var Navlist = document.querySelectorAll(".header-menu a");
 	var Aboutlist = document.querySelectorAll('.header-about li a');	 
 	for(var i = 0; i < Navlist.length; i++) {
 		Navlist[i].onclick = function() {
