86681aef78bf7ef14b6f1df105aacda806f1069f
Varjson <json_key@163.com>
2018-02-10 15:05:36 +0800

    2 10

--- new/js/video.js
+++ new/js/video.js
@@ -1,7 +1,6 @@
 
 var navList = document.querySelectorAll('.navbar li a');
-var itemList = document.querySelectorAll('.star .item');
-console.log(navList);
+var itemList = document.querySelectorAll('.star .item_');
 for(var i =0 ;i < navList.length; i++){
 	navList[i].onclick = function(){
 		//菜单颜色和背景色的切换
@@ -12,6 +11,7 @@ for(var i =0 ;i < navList.length; i++){
 		this.className += 'menu-active';
 		//菜单对应内容的切换
 		for(var j = 0 ;j< itemList.length; j++){
+			itemList[j].style.display = 'none';
 			if(navList[j] == this){
 				itemList[j].style.display = 'block';
 			}else{
